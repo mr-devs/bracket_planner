214ea9190477a416048090d75ec15afec366c056
Matthew R. DeVerna <48068852+mr-devs@users.noreply.github.com>
2022-05-10 09:06:46 -0400

    save tinkering to handle uploaded file - not yet ready

--- web_interface/js/script.js
+++ web_interface/js/script.js
@@ -4,3 +4,13 @@ function shuffleArray(arr) {
     const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
+}
+
+const inputElement = document.getElementById("uploadFile");
+inputElement.addEventListener("change", handleFiles, false);
+
+function handleFiles() {
+  console.log("read files")
+  const fileList = this.files; /* now you can work with the file list */
+  console.log(fileList)
+}
